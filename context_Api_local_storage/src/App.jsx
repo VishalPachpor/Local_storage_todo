a5450f4d6abe2c1e04fa6542a46b392edbe63fa0
Vishal Pachpor <vishalpachpor@Vishals-MacBook-Air.local>
2023-11-15 15:21:42 +0530

    Functionality added

--- context_Api_local_storage/src/App.jsx
+++ context_Api_local_storage/src/App.jsx
@@ -2,62 +2,64 @@ import { useEffect, useState } from "react";
 import { ToDoProvider } from "./context/Todo";
 
 function App() {
-  const [todos, setToDo] = useState([]); 
+  const [todos, setToDo] = useState([]);
 
   const addToDo = (todo) => {
-    setToDo((prev)=>[{id:Date.now(), ...todo}, ...prev])
-  }
+    setToDo((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
   const deleteToDo = (id) => {
-    setToDo((prev)=> prev.filter((todo)=>todo.id !== id));
-  }
+    setToDo((prev) => prev.filter((todo) => todo.id !== id));
+  };
 
-  const updateToDo = (id, todo) =>{
-    setToDo((prev)=>{
-      return prev.map((prevTodo)=>{
-        if(prevTodo.id === id){
+  const updateToDo = (id, todo) => {
+    setToDo((prev) => {
+      return prev.map((prevTodo) => {
+        if (prevTodo.id === id) {
           return todo;
-        }else{
+        } else {
           return prevTodo;
         }
       });
     });
-  }
+  };
 
-  const toggleComplete = (id) =>{
+  const toggleComplete = (id) => {
     // setToDo((prev)=> prev.map((prevTodo)=> prevTodo === id ? {...prevTodo, completed : !prevTodo.completed}: prevTodo ));
 
     setToDo((prev) => {
-      return prev.map((prevTodo)=>{
-        if(prevTodo.id===id){
-          return {...prevTodo, completed : !prevTodo.completed};
-        }else{
-         return prevTodo
+      return prev.map((prevTodo) => {
+        if (prevTodo.id === id) {
+          return { ...prevTodo, completed: !prevTodo.completed };
+        } else {
+          return prevTodo;
         }
-      })
-    })
-  }
+      });
+    });
+  };
   // getting teh data in local storage
-  useEffect(()=>{
-      const todos = JSON.parse(localStorage.getItem("todos"))
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
 
-      if(todos && todos.length  >0){
-        setToDo(todos);
-      }
-  },[])
+    if (todos && todos.length > 0) {
+      setToDo(todos);
+    }
+  }, []);
 
   // Setting the data in local storage.
-  useEffect(()=> {
-    localStorage.setItem("todos", JSON.stringify(todos))
-  }, [todos])
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
-    <ToDoProvider value={{todos, addToDo, deleteToDo, toggleComplete, updateToDo}}>
+    <ToDoProvider
+      value={{ todos, addToDo, updateToDo, deleteToDo, toggleComplete }}
+    >
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-          <h1 className="text-2xl font-bold text-center items-center relative mb-8 mt-2">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
+
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
           </div>
---
+++ context_Api_local_storage/src/components/index.js
@@ -0,0 +1,4 @@
+import ToDOForm from "./ToDOForm"
+import TodoItems from "./TodoItems"
+
+export {ToDOForm, TodoItems} 
